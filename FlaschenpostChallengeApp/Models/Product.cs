ec7732d652a4aae3049e65b053a672a8cdf82ab3
Sebastian Dombrowsky <sebdombrowsky@web.de>
2022-05-31 16:18:33 +0200

    App angelegt

---
+++ FlaschenpostChallengeApp/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlaschenpostChallengeApp.Models
+{
+  public class Product
+  {
+    public Product()
+    {
+    }
+
+
+  }
+}
